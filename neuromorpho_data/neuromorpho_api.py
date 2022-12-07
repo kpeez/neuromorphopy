85bf996f534b876e8c115a4c9d1013d1e4600611
kpuhger <krpuhger@gmail.com>
2022-12-07 13:51:19 -0800

    feat: created NeuroMorpho class
    - Wrapped functions into NeuroMorpho class
    - added validation of search queries
    - added export of metadata and swc data

--- neuromorpho_data/neuromorpho_api.py
+++ neuromorpho_data/neuromorpho_api.py
@@ -1,7 +1,11 @@
 """Work with NeuroMorpho API."""
+from __future__ import annotations
+
 import contextlib
 import io
+import pickle
 import re
+from pathlib import Path
 from typing import Any
 
 import numpy as np
@@ -110,6 +114,29 @@ def request_url_post(query: dict[str, str], **kwargs: Any) -> requests.models.Re
     return page
 
 
+def get_query_fields() -> set[str]:
+    """Get list of accepted search fields."""
+    return set(request_url_get(f"{NEUROMORPHO_API}/neuron/fields").json()["Neuron Fields"])
+
+
+def get_query_values(field: str) -> set[str]:
+    """Get list of accepted values for a given search field."""
+    return set(request_url_get(f"{NEUROMORPHO_API}/neuron/fields/{field}").json()["fields"])
+
+
+def validate_search(query: dict[str, str]) -> None:
+    """Validate search query to ensure field and its items are acceptable."""
+    query_fields = get_query_fields()
+    print("Validating search query...")
+    if not set(query.keys()).issubset(query_fields):
+        raise ValueError(f"Invalid search field: {set(query.keys()) - query_fields}")
+
+    for field, values in query.items():
+        query_values = get_query_values(field)
+        if not set(values).issubset(query_values):
+            raise ValueError(f"Invalid field values for {field}: {set(values) - query_values}")
+
+
 def _get_swc_url(neuron_name: str) -> str:
     """Get URL for a neuron's swc data from NeuroMorpho archives."""
     neuron_url = f"{NEURON_INFO}{neuron_name}"
@@ -121,83 +148,130 @@ def _get_swc_url(neuron_name: str) -> str:
     return f"{NEUROMORPHO}/{match}"
 
 
-def get_neuron_swc(neuron_name: str) -> pd.DataFrame:
+class NeuroMorpho:
     """
-    Create DataFrame of swc data for neuron using neuron_name.
+    Access NeuroMorpho API to get neuron reconstructions.
 
-    Args:
-        neuron_name (str): name of neuron
+    Attributes:
+        valid_field_names (set): Set containing the valid field names to use for a query.
+        neuron_list (list): List of neuron metadata in json
 
-    Returns:
-        pd.DataFrame: swc data
     """
-    swc_resp = request_url_get(_get_swc_url(neuron_name))
-    response_text = io.StringIO(swc_resp.text)
-    response_list = response_text.readlines()
-    num_lines = next(idx for idx, line in enumerate(response_list) if "#" not in line)
 
-    raw_swc_data = pd.DataFrame(response_list[num_lines:])
-    swc_data = raw_swc_data[0].str.replace("\r\n", "").str.split(expand=True)
-    col_names = dict(zip(swc_data.columns, ["id", "type", "x", "y", "z", "radius", "parent"]))
-    swc_data.rename(columns=col_names, inplace=True)
-    # set dtypes
-    swc_data = swc_data.astype(
-        {
-            "id": int,
-            "type": int,
-            "x": float,
-            "y": float,
-            "z": float,
-            "radius": float,
-            "parent": int,
-        }
-    )
+    def __init__(self) -> None:
+        if check_api_status():
+            self.valid_field_names: set[str] = get_query_fields()
+            # self.neuron_list: list[dict[str, Any]] = []
+            self.neuron_metadata: pd.DataFrame = pd.DataFrame()
+            self.swc_data: dict[str, pd.DataFrame] = {}
 
-    return swc_data
+        else:
+            raise ConnectionError("The NeuroMorpho API is currently down.")
 
+    def get_field_values(self, field: str) -> set[str]:
+        """
+        Get all possible values for a given field.
 
-def get_neuron_list(query: dict[str, str]) -> list[dict]:
-    """
-    Get list of neurons from a search query.
+        Args:
+            field (str): field name
 
-    See list of query keys here: https://neuromorpho.org/api/neuron/fields
+        Returns:
+            set[str]: Valid field values to use for search queries.
+        """
+        return get_query_values(field)
 
-    Args:
-        query (dict[str, str]): query values to filter neurons
+    def get_neuron_metadata(self, query: dict[str, str]) -> pd.DataFrame:
+        """
+        Get list of neurons from a search query.
 
-    Returns:
-        list[dict]: list of dicts where each dict is a single neuron's metadata.
-    """
-    # use an initial request to get query info
-    total_neurons = request_url_post(query).json()["page"]["totalElements"]
-    num_pages = np.ceil(total_neurons / MAX_NEURONS).astype(int)
+        See list of query keys here: https://neuromorpho.org/api/neuron/fields
 
-    neuron_list = []
-    for page_idx in range(num_pages):
-        neuron_count = MAX_NEURONS
-        page = request_url_post(query, params={"size": neuron_count, "page": page_idx})
-        neuron_list.extend(page.json()["_embedded"]["neuronResources"])
+        Args:
+            query (dict[str, str]): query values to filter neurons
 
-    return neuron_list
+        Returns:
+            list[dict]: list of dicts where each dict is a single neuron's metadata.
+        """
+        validate_search(query)
+        print("Beginning search...")
+        # use an initial request to get query info
+        total_neurons = request_url_post(query).json()["page"]["totalElements"]
+        num_pages = np.ceil(total_neurons / MAX_NEURONS).astype(int)
 
+        neuron_list = []
+        for page_idx in range(num_pages):
+            neuron_count = MAX_NEURONS
+            page = request_url_post(query, params={"size": neuron_count, "page": page_idx})
+            neuron_list.extend(page.json()["_embedded"]["neuronResources"])
 
-def get_swc_data(neuron_list: list[str]) -> dict[str, pd.DataFrame]:
-    """
-    Look up neuron name and retrieve swc data from NeuroMorpho.
+        self.neuron_list = neuron_list
+        self.neuron_metadata = pd.DataFrame(neuron_list)
 
-    Args:
-        neuron_list (list[str]): list of neuron names
+        return self.neuron_metadata
 
-    Returns:
-        dict[str, pd.DataFrame]: swc data for each neuron in the input list.
-    """
-    print(f"Getting swc data for {len(neuron_list)} neurons.")
+    def get_neuron_swc(self, neuron_name: str) -> pd.DataFrame:
+        """
+        Create DataFrame of swc data for neuron using neuron_name.
 
-    swc_data: dict[str, pd.DataFrame] = {}
-    for n, neuron in enumerate(neuron_list):
-        if n % 100 == 0:
-            print(f"loading neuron: {n}")
-        with contextlib.suppress(ValueError):
-            swc_data[neuron] = get_neuron_swc(neuron)
+        Args:
+            neuron_name (str): name of neuron
 
-    return swc_data
+        Returns:
+            pd.DataFrame: swc data
+        """
+        swc_resp = request_url_get(_get_swc_url(neuron_name))
+        response_text = io.StringIO(swc_resp.text)
+        response_list = response_text.readlines()
+        num_lines = next(idx for idx, line in enumerate(response_list) if "#" not in line)
+
+        raw_swc_data = pd.DataFrame(response_list[num_lines:])
+        swc_data = raw_swc_data[0].str.replace("\r\n", "").str.split(expand=True)
+        col_names = dict(zip(swc_data.columns, ["id", "type", "x", "y", "z", "radius", "parent"]))
+        swc_data.rename(columns=col_names, inplace=True)
+        # set dtypes
+        swc_data = swc_data.astype(
+            {
+                "id": int,
+                "type": int,
+                "x": float,
+                "y": float,
+                "z": float,
+                "radius": float,
+                "parent": int,
+            }
+        )
+
+        return swc_data
+
+    def download_swc_data(self) -> None:
+        """Look up neuron name and retrieve swc data from NeuroMorpho."""
+        assert self.neuron_metadata.empty is False, "No metadata!"
+
+        print(f"Downloading swc data for {len(self.neuron_metadata['neuron_name'])} neurons...")
+
+        for n, neuron in enumerate(self.neuron_metadata["neuron_name"]):
+            if n % 100 == 0:
+                print(f"loading neuron: {n}")
+            with contextlib.suppress(ValueError):
+                self.swc_data[neuron] = self.get_neuron_swc(neuron_name=neuron)
+
+    def export_metadata(self, data_path: str, query_name: str) -> None:
+        """
+        Export metadata as csv.
+
+        Args:
+            data_path (str): export path
+            query_name (str): name of metadata file
+        """
+        self.neuron_metadata.to_csv(f"{Path(data_path)}/{query_name}.csv", index=False)
+
+    def export_swc_data(self, data_path: str, swc_filename: str) -> None:
+        """
+        Export dict of swc data to pkl file.
+
+        Args:
+            data_path (str): export path
+            swc_filename (str): name of pkl file
+        """
+        with open(f"{Path(data_path)}/{swc_filename}.pkl", "wb") as f:
+            pickle.dump(self.swc_data, f, protocol=-1)
