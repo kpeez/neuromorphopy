37bb4cd87b10351972d954d5dc15b3cfb72d9943
kpuhger <krpuhger@gmail.com>
2023-01-07 10:28:15 -0800

    update imports

--- neuromorphopy/__init__.py
+++ neuromorphopy/__init__.py
@@ -1,4 +1,6 @@
 """NeuroMorpho API."""
 __version__ = "0.0.1"
 
-from .neuromorpho_api import NeuroMorpho, get_image_url
+from .neuromorpho_api import NeuroMorpho
+from .utils import get_image_url
+from .swc import get_neuron_swc, download_swc_data
