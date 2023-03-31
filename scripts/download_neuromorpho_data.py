395040ed6a232b735d458cf8e6c8dbeedf8b1100
kpuhger <krpuhger@gmail.com>
2023-03-31 16:59:03 -0700

    update for new swc download method

--- scripts/download_neuromorpho_data.py
+++ scripts/download_neuromorpho_data.py
@@ -13,13 +13,13 @@ def config_args() -> argparse.Namespace:
         "-q",
         "--query_file",
         type=str,
-        help="Path to query file to search for neurons on neuromorpho.org.",
+        help="Path to query file to search for neurons on NeuroMorpho.",
     )
     parser.add_argument(
         "-e",
         "--export_path",
         type=str,
-        help="Path to save exported data.",
+        help="Path to export metadata and swc files to.",
     )
     return parser.parse_args()
 
@@ -30,13 +30,9 @@ if __name__ == "__main__":
     query = load_json_query(args.query_file)
     nm = NeuroMorpho(query)
 
-    print("Downloading metadata from neuromorpho.org...")
     metadata_filename = f"{Path(args.query_file).stem}_metadata.csv"
     nm.export_metadata(args.export_path, metadata_filename)
-
-    print("Download SWC data...")
-    nm.download_query_swc()
-    swc_filename = f"{Path(args.query_file).stem}_swc.pkl"
-    nm.export_swc_data(args.export_path, swc_filename)
+    print("Finished downloading metadata.")
+    nm.download_query_swc(args.export_path)
 
     print(f"Finished downloading data from {args.query_file}.")
