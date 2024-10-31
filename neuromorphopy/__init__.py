847a4357b84052f02356986078375232cb866b20
kpuhger <krpuhger@gmail.com>
2024-10-30 17:43:02 -0700

    update imports

--- neuromorphopy/__init__.py
+++ neuromorphopy/__init__.py
@@ -1,6 +1,16 @@
-"""NeuroMorpho API."""
-__version__ = "0.0.1"
+"""NeuroMorpho.org API client library"""
 
-from .neuromorpho_api import NeuroMorpho
-from .swc import download_swc_data, get_neuron_swc
-from .utils import get_image_url, load_json_query
+from .client import NeuroMorphoClient, search_and_download
+from .exceptions import ApiError, NeuroMorphoError, ValidationError
+from .query import QueryBuilder
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "ApiError",
+    "NeuroMorphoClient",
+    "NeuroMorphoError",
+    "QueryBuilder",
+    "ValidationError",
+    "search_and_download",
+]
