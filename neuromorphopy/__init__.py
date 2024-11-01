0c26111578172db50e76b71a96090c4d09e0ebeb
Kyle Puhger <krpuhger@gmail.com>
2024-10-31 17:54:40 -0700

    bump version to 0.1.1 (#13)
    * parse version from package
    
    * bump version

--- neuromorphopy/__init__.py
+++ neuromorphopy/__init__.py
@@ -1,10 +1,12 @@
 """NeuroMorpho.org API client library"""
 
+from importlib.metadata import version
+
 from .api import NeuroMorphoClient, search_and_download
 from .exceptions import ApiError, NeuroMorphoError, ValidationError
 from .query import Query, QueryFields
 
-__version__ = "0.1.0"
+__version__ = version("neuromorphopy")
 
 __all__ = [
     "ApiError",
